e90d581dc9b291449fe2594cc2cd5b707654a977
Marcin Bielak <marcin.bieli@gmail.com>
2016-01-27 23:57:24 +0100

    add HOW TO with first script arguments parser

--- README.md
+++ README.md
@@ -14,15 +14,16 @@ HOW TO RUN ANALYZER
 ```
 $ git clone https://github.com/bieli/egzopix__digital_images_analyzer.git
 $ cd egzopix__digital_images_analyzer/
-$ python3.4 egzopix/analyzer.py --concept=extracting_image_detail --input=/tmp/123.png --output=/tmp/ --verbose
-...
+$ python egzopix/analyzer.py -c=create_images_blocks_10x10 -i=/tmp/123.jpg -o=/tmp/image_blocks_10x10/ -v=DEBUG
+[VERBOSE MODE] concept: 'create_images_blocks_10x10' input: '<open file '/tmp/123.jpg', mode 'r' at 0x7f94ff2070c0>' output: '/tmp/image_blocks_10x10/' verbose: 'DEBUG' 
+Hello, world !
 
 ```
 
 TODO
 ----
 - [x] add conceptual image and first README
-- [ ] add HOW TO with first script arguments parser
+- [x] add HOW TO with first script arguments parser
 - [ ] add first runnable block code
 - [ ] add travis CI
 - [ ] add examples input images and outputs from analyzer
--- egzopix/analyzer.py
+++ egzopix/analyzer.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3.4
 
+import argparse
+
+parser = argparse.ArgumentParser(description='EGZOPIX - digilat images analyzers.')
+parser.add_argument('-c', '--concept', type=str, 
+                    help='concept name')
+parser.add_argument('-i', '--input', type=argparse.FileType('r'),
+                   help='input original image path')
+parser.add_argument('-o', '--output', type=str,
+                   help='outtput changed image(s) path')
+parser.add_argument('-v', '--verbose', type=str, default='INFO',
+                   help='verbose level [DEBUG|INFO|WARNING|ERROR] (default: INFO)')
+
+args = parser.parse_args()
+
+if args.verbose and args.verbose == 'DEBUG':
+  print("[VERBOSE MODE] concept: '{}' input: '{}' output: '{}' verbose: '{}' ".format(
+         args.concept,
+         args.input,
+         args.output,
+         args.verbose
+         ))
+
 print("Hello, world !")
 
